59738d90bae2dc2d9c4150d49713bd357a65f0ff
tzlilhaim <tzlilhaim480@gmail.com>
2020-10-12 16:36:07 +0300

    basic react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./styles/App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Agora Market</h2>
+    </div>
+  )
+}
+
+export default App
